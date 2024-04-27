277543492d7a722671104a3e5f143039207b2c50
Bpaul311 <bizimapaul777@gmail.com>
2024-04-27 03:34:17 -0700

    removed comment

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -6,17 +6,18 @@ of the web_static folder of your AirBnB Clone repo, using the function do_pack
 from fabric.api import *
 from datetime import datetime
 
+
 def pack():
+
     """
     Making an archive on a web server
     """
 
-    timestamp = datetime.now()
+    time = datetime.now()
 
-    archive = 'webstatic_' + timestamp.strftime("%Y%m%d%H%M%S") + '.' + 'tgz'
+    archive = 'webstatic_' + time.strftime("%Y%m%d%H%M%S") + '.' + 'tgz'
     local('mkdir -p versions')
     create = local('tar -cvzf versions / {} web_static'.format(archive))
-    
     if create is None:
         return None
     else:
